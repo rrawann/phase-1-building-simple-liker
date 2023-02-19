64a45bb4717670648b64081e0ac2ff2bdd2afe76
Rawan <rawnq.sb@gmail.com>
2023-02-19 13:03:21 +0300

    Completed Assignment

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+document.getElementById("modal").className = "hidden"
+document.addEventListener("DOMContentLoaded", () => {
+  const EMPTY_HEART = '♡'
+  const FULL_HEART = '♥'
+  
+  
+  
+  // Your JavaScript code goes here!
+  // Grabs the like-glyph (heart icon)
+  const like = document.querySelectorAll("span.like-glyph")
+  //const like2 = document.getElementById("201811190.span.like-glyph")
+  //console.log(like2)
+  
+  like.forEach(thing => thing.addEventListener("click", changeHeart))
+  //like2.addEventListener("click", changeHeart)
+  console.log(like.target)
 
-// Your JavaScript code goes here!
-
-
+  function changeHeart(heart){
+    //change the heart from empty to full, 
+    //if heart is full, click changes to empty and vice versa
+    mimicServerCall()
+   
+    .then(() => {
+      console.log("there is a click", heart.target.textContent)
+      if(heart.target.textContent === EMPTY_HEART){
+        heart.target.textContent = FULL_HEART
+      }else if(heart.target.textContent === FULL_HEART){
+        heart.target.textContent = EMPTY_HEART
+      }
+    })
+    .catch(() => {
+      const modal1 = document.getElementById("modal")
+      console.log("there was an error")
+      modal1.className = "show"   
+    setTimeout(() => {
+      modal1.className = "hidden"}, 3000)
+    })
+    }
+  })
 
 
 //------------------------------------------------------------------------------
